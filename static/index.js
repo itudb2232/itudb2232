5de9d9ecde78af8679121a79a7cfe20f8311c7f2
turkbayrakalp <turkbayrak20@itu.edu.tr>
2022-12-04 16:25:46 +0300

    Frontend development has begun
    - Added a navigation bar to various pages of the site.
    - Integrated placeholder pages for launches, launchpads, ships, rockets and home
    - Rendered html templates for each different page.

---
+++ static/index.js
@@ -0,0 +1,2 @@
+// this javascript is already linked in base.html
+// when needed use this file for your js codes
--- website/views.py
+++ website/views.py
@@ -1,28 +1,27 @@
 # This file is for arranging which pages the user can navigate to (i.e. launchpads, launches, rockets, ships...) #
-# this file is a blueprint, it has a bunch of roots and urls inside of it #
+# This file is a blueprint, it has a bunch of roots and urls inside of it #
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
-# The functions below will return html files later on, these h1 tags are just placeholders #
 
 @views.route('/')
 def home():
-    return "<h1 align = 'center'>You have landed on our webpage!</h1>"
+    return render_template("home.html")
 
 @views.route('/launches')
 def launches():
-    return "<h1>Launches Page</h1>"
+    return render_template("launches.html")
 
 @views.route('/launchpads')
 def launchpads():
-    return "<h1>Launchpads Page</h1>"
+    return render_template("launchpads.html")
 
 @views.route('/rockets')
 def rockets():
-    return "<h1>Rockets Page</h1>"
+    return render_template("rockets.html")
 
 @views.route('/ships')
 def ships():
-    return "<h1>Ships Page</h1>"
+    return render_template("ships.html")
