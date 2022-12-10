b6e527ef7a8760b66451ef16bd0798c60cf44051
ituitis-emul19 <emul19@itu.edu.tr>
2022-12-10 12:02:34 +0300

    Added DB insert functionality for payloads

--- website/views.py
+++ website/views.py
@@ -4,6 +4,9 @@
 from flask import Blueprint, render_template, current_app, request
 import sqlite3
 
+from time import time_ns
+from random import random
+
 views = Blueprint('views', __name__)
 
 
@@ -45,11 +48,27 @@ def ship_details_2():
 
 @views.route('/payloads', methods=['GET', 'POST'])
 def payloads():
-    con = sqlite3.connect(current_app.config["db"])
-    con.row_factory = sqlite3.Row  # Query returns objects
-    cur = con.cursor() 
-    payloads = cur.execute("SELECT * FROM payloads").fetchall()
-    insert_data = request.form
-    print(insert_data)
-    return render_template("payloads.html", payloads=payloads)
+    if request.method == "GET":
+        con = sqlite3.connect(current_app.config["db"])
+        con.row_factory = sqlite3.Row  # Query returns objects
+        cur = con.cursor() 
+        payloads = cur.execute("SELECT * FROM payloads").fetchall()
+        insert_data = request.form
+        print(insert_data)
+        return render_template("payloads.html", payloads=payloads)
+    else:
+        con = sqlite3.connect(current_app.config["db"])
+        con.row_factory = sqlite3.Row  # Query returns objects
+        cur = con.cursor() 
+
+        # Add new payload
+        n = request.form
+        new_tuple = (str(time_ns()) + str(random())[-4:],  # ID Generator (our data's ID's were varchars to begin with)
+                    n["name"], n["type"], n["reused"], n["manufacturer"], n["mass_kg"], n["mass_lb"], n["orbit"], n["reference-system"], n["regime"])
+
+        cur.execute('INSERT INTO payloads VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', new_tuple)
+        con.commit()
+
+        payloads = cur.execute("SELECT * FROM payloads").fetchall()
+        return render_template("payloads.html", payloads=payloads)
 
