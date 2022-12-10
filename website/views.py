e6f4518a6cef3161a2ea80cdb3454a7a92cce036
turkbayrakalp <turkbayrak20@itu.edu.tr>
2022-12-10 10:11:03 +0300

    Post Method Added

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 # This file is for arranging which pages the user can navigate to (i.e. launchpads, launches, rockets, ships...) #
 # This file is a blueprint, it has a bunch of roots and urls inside of it #
 
-from flask import Blueprint, render_template, current_app
+from flask import Blueprint, render_template, current_app, request
 import sqlite3
 
 views = Blueprint('views', __name__)
@@ -11,15 +11,15 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html")
 
-@views.route('/launches')
+@views.route('/launches', methods=['GET', 'POST'])
 def launches():
     return render_template("launches.html")
 
-@views.route('/launchpads')
+@views.route('/launchpads', methods=['GET', 'POST'])
 def launchpads():
     return render_template("launchpads.html")
 
-@views.route('/rockets')
+@views.route('/rockets', methods=['GET', 'POST'])
 def rockets():
     con = sqlite3.connect(current_app.config["db"])
     con.row_factory = sqlite3.Row  # Query returns objects
@@ -27,14 +27,16 @@ def rockets():
     rockets = cur.execute("SELECT * FROM rockets").fetchall()
     return render_template("rockets.html", rockets=rockets)
 
-@views.route('/ships')
+@views.route('/ships', methods=['GET', 'POST'])
 def ships():
     return render_template("ships.html")
 
-@views.route('/payloads')
+@views.route('/payloads', methods=['GET', 'POST'])
 def payloads():
     con = sqlite3.connect(current_app.config["db"])
     con.row_factory = sqlite3.Row  # Query returns objects
     cur = con.cursor() 
     payloads = cur.execute("SELECT * FROM payloads").fetchall()
+    insert_data = request.form
+    print(insert_data)
     return render_template("payloads.html", payloads=payloads)
