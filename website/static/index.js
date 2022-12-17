730d3fbef395465670e900cb33ac2499f89e4e8f
ituitis-emul19 <emul19@itu.edu.tr>
2022-12-17 17:55:21 +0300

    Added JS functions for delete functionality

--- website/static/index.js
+++ website/static/index.js
@@ -1,2 +1,12 @@
 // this javascript is already linked in base.html
 // when needed use this file for your js codes
+
+function delete_rocket(rocket_id) {
+    document.location = '/delete_rocket?rocket_id=' + rocket_id
+}
+function delete_rocket_d1(rocket_id) {
+    document.location = '/delete_rocket_detail_1?rocket_id=' + rocket_id
+}
+function delete_rocket_d2(rocket_id) {
+    document.location = '/delete_rocket_detail_2?rocket_id=' + rocket_id
+}
