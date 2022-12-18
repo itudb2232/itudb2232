85b842ee455524e2e224c6bc2be1edd93df6402c
yamanmetehan <metehan1861@gmail.com>
2022-12-18 23:45:09 +0300

    Added delete funtion to ships

--- website/static/index.js
+++ website/static/index.js
@@ -28,3 +28,7 @@ function delete_payload(payload_id) {
 function delete_launch(launch_id) {
     document.location = '/delete_launch?launch_id=' + launch_id
 }
+//SHIPS//
+function delete_ship(ship_id) {
+    document.location = '/delete_ship?ship_id=' + ship_id
+}
